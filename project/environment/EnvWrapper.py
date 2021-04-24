9d1dd5876881560845ef4c68f1f655f9f2a095c9
s194591 <s194591@student.dtu.dk>
2021-04-24 11:39:29 +0200

    EnvWrapper step action redefined

--- project/environment/EnvWrapper.py
+++ project/environment/EnvWrapper.py
@@ -1,4 +1,5 @@
 import gym
+import torch
 from torchvision import transforms
 
 
@@ -13,10 +14,18 @@ class EnvWrapper(gym.Env):
             transforms.Normalize(0.5, 0.5)
         ])
 
+    # Take 'step_size' steps with the same action
     def step(self, action):
-        obs, reward, done, info = self.env.step(action)
-        frame = self.transformer(obs)
-        return frame, reward, done, info
+
+        seq = torch.zeros(self.step_size, 64, 64)
+
+        for i in range(self.step_size):
+            obs, reward, done, info = self.env.step(action)
+            frame = self.transformer(obs)
+            frame = frame.squeeze()
+            seq[i] = frame
+
+        return seq
 
     def reset(self):
         return self.env.reset()
--- project/train.py
+++ project/train.py
@@ -1,9 +1,16 @@
+from project.agents.PPOAgent import PPOAgent
+from project.environment.EnvWrapper import EnvWrapper
+from project.models.policy_models import PolicyModel
+
 if __name__ == "__main__":
-    # env = EnvWrapper('procgen:procgen-starpilot-v0', 1)
-    env = gym.make('procgen:procgen-starpilot-v0')
+    env = EnvWrapper('procgen:procgen-starpilot-v0', 4)
+    # env = gym.make('procgen:procgen-starpilot-v0')
 
-    actor = PolicyModel(64, 64, env.action_space.n, True)
-    critic = PolicyModel(64, 64, 1, False)
+    # actor = PolicyModel(64, 64, env.env.action_space.n, True)
+    # critic = PolicyModel(64, 64, 1, False)
 
-    actorCritic = PPOAgent(env, actor, critic)
-    actorCritic.train()
+    seq_frames = env.step(env.env.action_space.sample())
+    print(seq_frames.size)
+
+    # actorCritic = PPOAgent(env, actor, critic)
+    # actorCritic.train()

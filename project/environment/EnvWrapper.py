32a902971b4134097240ee5fb58bfb197aad62e2
s194591 <s194591@student.dtu.dk>
2021-04-24 12:34:05 +0200

    Train step with buf_seq

--- project/environment/EnvWrapper.py
+++ project/environment/EnvWrapper.py
@@ -2,11 +2,14 @@ import gym
 import torch
 from torchvision import transforms
 
+from utils.MemBuffer import MemBuffer
+
 
 class EnvWrapper(gym.Env):
     def __init__(self, environment, step_size=4):
         self.step_size = step_size
         self.env = gym.make(environment)
+        self.mem_buffer = MemBuffer(batch_size=self.step_size)
         self.transformer = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Grayscale(),
@@ -18,14 +21,20 @@ class EnvWrapper(gym.Env):
     def step(self, action):
 
         seq = torch.zeros(self.step_size, 64, 64)
+        buf_seq = []
+        contains_done = False
 
         for i in range(self.step_size):
             obs, reward, done, info = self.env.step(action)
             frame = self.transformer(obs)
             frame = frame.squeeze()
             seq[i] = frame
+            buf_seq.append((frame, action, reward, done))
+            if done:
+                contains_done = True
+                continue
 
-        return seq
+        return seq, buf_seq, contains_done
 
     def reset(self):
         obs = self.env.reset()
--- utils/MemBuffer.py
+++ utils/MemBuffer.py
@@ -5,6 +5,11 @@ class MemBuffer():
         self.mem = []
         self.batch_size = batch_size
 
+    def add(self, observation, action, reward, done):
+        self.mem.append((observation, action, reward, done))
+        if len(self.mem) > self.mem_size:
+            self.mem = self.mem[-self.mem_size:]
+
     def __len__(self):
         return len(self.mem)
 

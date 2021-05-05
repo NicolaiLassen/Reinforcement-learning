6c54adeeddc0180a7b403f8926ca3510262a5583
s194591 <s194591@student.dtu.dk>
2021-05-05 22:48:28 +0200

    Added next states to mem buffer - yet to implement in agent

--- utils/mem_buffer.py
+++ utils/mem_buffer.py
@@ -12,16 +12,18 @@ class MemBuffer:
         self.action_space_n = action_space_n
 
         self.states = torch.zeros(self.max_length, self.motion_blur, self.height, self.width, dtype=torch.float).cuda()
+        self.next_states = torch.zeros(self.max_length, self.motion_blur, self.height, self.width, dtype=torch.float).cuda()
         self.rewards = torch.zeros(self.max_length, dtype=torch.float)
         self.actions = torch.zeros(self.max_length, dtype=torch.int).cuda()
         self.action_log_probs = torch.zeros(self.max_length, action_space_n, dtype=torch.float).cuda()
         self.done = torch.zeros(self.max_length, dtype=torch.int)
 
-    def set_next(self, state, reward, action, action_log_prob, done, mask=None):
+    def set_next(self, state, next_state, reward, action, action_log_prob, done, mask=None):
         if self.max_length == self.t:
             print("DON'T JUST TAKE ALL OF MY SPACE, YOU SON OF A GUN!")
             return
         self.states[self.t] = state
+        self.next_states[self.t] = next_state
         self.rewards[self.t] = float(reward)
         self.actions[self.t] = action
         self.action_log_probs[self.t] = action_log_prob
@@ -31,6 +33,7 @@ class MemBuffer:
     def clear(self):
         self.t = 0
         self.states = torch.zeros(self.max_length, self.motion_blur, self.height, self.width, dtype=torch.float).cuda()
+        self.next_states = torch.zeros(self.max_length, self.motion_blur, self.height, self.width, dtype=torch.float).cuda()
         self.rewards = torch.zeros(self.max_length, dtype=torch.float)
         self.actions = torch.zeros(self.max_length, dtype=torch.int).cuda()
         self.action_log_probs = torch.zeros(self.max_length, self.action_space_n, dtype=torch.float).cuda()

91aff4b7c93e2e06b29143cc9f6feb358e1b1187
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2021-05-09 15:49:25 +0200

    VIT,CONV test

--- project/main.py
+++ project/main.py
@@ -14,38 +14,9 @@ def create_dir(directory):
         os.makedirs(directory)
 
 
-def test_agent_ppo():
-    # TODO Test for each env start_0..200
-    for i in range(200):
-        env = EnvWrapper('procgen:procgen-starpilot-v0', start_level=i, num_levels=1)
-        width = 64
-        height = 64
-        actor = PolicyModelConv(width, height, env.env.action_space.n).cuda()
-        agent = PPOAgent(env, actor)
-        agent.load_actor("ckpt_ppo/actor_2000.ckpt")
-        s1 = env.reset()
-        rewards = []
-        steps_before_done = 0
-        while True:
-            s = s1
-            steps_before_done += 1
-            action, _, _ = agent.act(s)
-            s1, r, d, _ = env.step(action)
-            rewards.append(r)
-            if d:
-                break
-        print("Level: {}, N Steps: {}, Total Reward: {}".format(i, steps_before_done, sum(rewards)))
-
-
-def test_agent_dqn():
-    # TODO Test for each env 0..500
-    for i in range(500):
-        env = EnvWrapper('procgen:procgen-starpilot-v0', start_level=i, num_levels=1)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", default="vit", help="PPO model")
+    parser.add_argument("-m", "--model", default="conv", help="PPO model")
     args = parser.parse_args()
 
     print(args.model)
--- project/utils/plot.py
+++ project/utils/plot.py
@@ -5,7 +5,7 @@ import torch
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 if __name__ == '__main__':
-    rewards = torch.load('../ckpt_ppo/rewards.ckpt')
+    rewards = torch.load('../ckpt_ppo_conv/starpilot_easy/rewards.ckpt')
     print(rewards)
     print(len(rewards))
     print(len(rewards) * 2000)
